b97843e6420221db90deeb4397c5d1569611d2ed
nhurichie <richnhu@gmail.com>
2019-06-11 10:46:32 -0500

    changesin_public_index_file

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,7 @@ var $jobsList = $("#jobs_list");
 // The API object contains methods for each kind of request we'll make
 
 var API = {
-  saveJob: function(job) {
+  saveJob: function (job) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -17,27 +17,28 @@ var API = {
       data: JSON.stringify(job)
     });
   },
-  getJobs: function() {
+  getJobs: function () {
     return $.ajax({
       url: "api/jobs",
       type: "GET"
-    });    
+    });
   },
-  deleteJob: function(id) {
+  deleteJob: function (id) {
     return $.ajax({
-      url: "api/job/" + id,
+      url: "api/jobs/" + id,
       type: "DELETE"
     });
   }
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshJobs = function() {
-  API.getJobs().then(function(data) {
-    var $jobs = data.map(function(job) {
+var refreshJobs = function () {
+  API.getJobs().then(function (data) {
+    var $jobs = data.map(function (job) {
       var $a = $("<a>")
         .text(job.jobTitle)
-        .attr("href", "/job/" + job.job_id);      
+        .attr("href", "/job/" + job.job_id);
+
       var $li = $("<li>")
         .attr({
           class: "list-group-item",
@@ -62,18 +63,19 @@ var refreshJobs = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   console.log("Reached in handleSubmit");
   event.preventDefault();
 
   var job = {
     UserId: 1, //dummy for now
+    title: $jobsTitle.val().trim(),
     company: $jobsCompany.val().trim(), //dummy for now
+    description: $jobDescription.val().trim(),
     close_by: '1/1/2020',
     active: true, //always true on creation
     created_on: Date.now(),
-    title: $jobsTitle.val().trim(),
-    description: $jobDescription.val().trim()
+  
   };
 
   if (!(job.title && job.description)) {
@@ -82,7 +84,7 @@ var handleFormSubmit = function(event) {
   }
 
   console.log("save job:", job);
-  API.saveJob(job).then(function(data) {
+  API.saveJob(job).then(function () {
     refreshJobs;
   });
 
@@ -92,22 +94,22 @@ var handleFormSubmit = function(event) {
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  console.log("Reeached the delete");
+var handleDeleteBtnClick = function () {
+  console.log("Reached the delete");
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteJob(idToDelete).then(function() {
+  API.deleteJob(idToDelete).then(function () {
     refreshJobs;
   });
 };
 
 // // Add event listeners to the submit and delete buttons
- $(document).ready(function() {
+$(document).ready(function () {
   var $submitBtn = $("#submit");
 
-    $submitBtn.on("click", handleFormSubmit);
-    $jobsList.on("click", ".delete", handleDeleteBtnClick);
- });
+  $submitBtn.on("click", handleFormSubmit);
+  $jobsList.on("click", ".delete", handleDeleteBtnClick);
+});
 
